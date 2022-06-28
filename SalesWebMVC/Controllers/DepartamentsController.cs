baa09b002d2770f68d477bec70f3306dd3380611
Carlos-Salmentro <carlos.salmentro@hotmail.com>
2022-06-28 12:26:38 -0300

    First model controller view - Departament

---
+++ SalesWebMVC/Controllers/DepartamentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> departamentList = new List<Departament>();
+            departamentList.Add(new Departament { ID = 1, Name = "Eletronicos" });
+            departamentList.Add(new Departament { ID = 2, Name = "Fashion" });
+
+            return View(departamentList);
+        }
+    }
+}
---
+++ SalesWebMVC/Models/Departament.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Departament
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
